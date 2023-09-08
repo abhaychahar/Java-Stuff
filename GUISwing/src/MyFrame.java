b8f1aaf046c684d41ffc70813f3d9c0aa7e4b2eb
abhaychahar <abhaychahar.1602@gmail.com>
2023-09-08 18:21:19 +0530

    Labels

--- GUISwing/src/MyFrame.java
+++ GUISwing/src/MyFrame.java
@@ -5,17 +5,18 @@ import java.awt.Color;
 public class MyFrame extends JFrame {
 	
 	public MyFrame() {
-		// GUI window to add components to
-		this.setSize(800, 600); // set x-dimension and y-dimension of frame
-		this.setVisible(true); // make frame visible
-		this.setTitle("My GUI"); // set title of frame
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // exit application on close instead of hiding it
-		this.setResizable(false); // prevent frame from being resized
+		// Frame is a GUI window to add components to
 		
-		ImageIcon logo=new ImageIcon("src/Images/Logo.png"); // create image icon
-		this.setIconImage(logo.getImage()); // set icon of frame
+		this.setSize(800, 800); // Set x-dimension and y-dimension of frame
+		this.setVisible(true); // Make frame visible
+		this.setTitle("My GUI"); // Set title of frame
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Exit application on close instead of hiding it
+		this.setResizable(false); // Prevent frame from being resized
 		
-		this.getContentPane().setBackground(new Color(173, 95, 157)); // set background color of frame		
+		ImageIcon frameLogo=new ImageIcon("src/Images/Frame_Logo.png"); // Create image icon
+		this.setIconImage(frameLogo.getImage()); // Set icon of frame
+		
+		this.getContentPane().setBackground(new Color(173, 95, 157)); // Set background color of frame		
 	}
 	
 }
